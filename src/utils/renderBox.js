13e31fec8b14053197c84bb021400e71a84293f0
Allen Ai <78721047+YangyulinAi@users.noreply.github.com>
2022-06-13 13:35:41 +1000

    Update

--- src/utils/renderBox.js
+++ src/utils/renderBox.js
@@ -8,6 +8,10 @@ import labels from "./labels.json";
  * @param {Array} scores_data scores array
  * @param {Array} classes_data class array
  */
+const music = new Audio('public/alert.wav');
+var d = new Date();
+window.time =0;
+console.log(d);
 export const renderBoxes = (canvasRef, threshold, boxes_data, scores_data, classes_data) => {
   const ctx = canvasRef.current.getContext("2d");
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); // clean canvas
@@ -33,19 +37,39 @@ export const renderBoxes = (canvasRef, threshold, boxes_data, scores_data, class
       // Draw the bounding box.
      
       ctx.lineWidth = 2;
-      ctx.strokeRect(x1, y1, width, height);
 
+      //   ctx.fillStyle = "rgba(227,23,13,0.5)";
+      // ctx.fillRect(x1, y1, width, height);
       // Draw the label background.
 	  if(klass=="upright")
 	  {
-		  ctx.fillStyle = "#00FF00";
-		   ctx.strokeStyle = "#00FF00";
+          ctx.fillStyle = "rgba(63,227,13,0.4)";
+          ctx.fillRect(x1, y1, width, height);
+		  ctx.fillStyle = "rgba(63,227,13,1)";
+		   ctx.strokeStyle = "rgba(63,227,13,0.5)";
+          window.time = window.time - 10
+          if (window.time<0){
+              window.time=0;
+          }
 	  }
       else if (klass == "fall")
 	  {
-		  ctx.fillStyle = "#FF0000";
-		   ctx.strokeStyle = "#FF0000";
+          ctx.fillStyle = "rgba(227,23,13,0.4)";
+          ctx.fillRect(x1, y1, width, height);
+		  ctx.fillStyle = "rgba(227,23,13,1)";
+          ctx.strokeStyle = "rgba(227,23,13,0.5)";
+          window.time = window.time + 1
+          if (window.time >100){
+              music.play();
+              music.loop =true;
+              console.log(alert)
+          }
 	  }
+      if (window.time<100){
+          music.pause();
+      }
+      console.log(window.time)
+
       const textWidth = ctx.measureText(klass + " - " + score + "%").width;
       const textHeight = parseInt(font, 10); // base 10
       ctx.fillRect(x1 - 1, y1 - (textHeight + 2), textWidth + 2, textHeight + 2);
